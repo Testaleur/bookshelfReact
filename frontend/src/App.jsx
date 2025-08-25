ab7b19a68404d83c0a3ba1f0e7685e9499ab4c13
Testaleur <estebangalliot@gmail.com>
2025-08-25 18:56:50 +0200

    calls API and displays saved data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,24 @@ import './App.css'
 import Card from './components/card.jsx'
 import RowContainer from './components/rowContainer.jsx'
 import Shelf from './components/shelf.jsx'
+import { useEffect, useState } from 'react';
+
+const API_URL = 'http://localhost:4000/books';
 
 function App() {
+  // displayed features
+  const [books, setBooks] = useState([]);
+  // const [title, setTitle] = useState('');
+  // const [author, setAuthor] = useState('');
+
+  // Load books from backend
+  useEffect(() => {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(data => setBooks(data))
+      .catch(err => console.error('Error fetching books:', err));
+  }, []);
+
   return (
     <>
       <Card id = "mainTitle ">
@@ -14,8 +30,11 @@ function App() {
 
         <Card id = "bookshelvesContainer" title = "bookshelvesContainer">
           <Shelf>
-            <div>book1</div>
-            <div>book2</div>
+            <ul>
+              {books.map(book => (
+                <li key={"book" + book.id}>{book.title} — {book.author}</li>
+              ))}
+            </ul>
           </Shelf>
 
           <Shelf>
