0582711e22047e5140f289ad556e2f05dcb07638
Testaleur <estebangalliot@gmail.com>
2025-10-27 09:41:06 +0100

    new stats : number per states

--- frontend/src/components/options/stats.jsx
+++ frontend/src/components/options/stats.jsx
@@ -2,26 +2,43 @@ import Card from '../utils/card.jsx';
 import RowContainer from '../utils/rowContainer.jsx';
 import ColumnContainer from '../utils/columnContainer.jsx';
 import DisplayData from '../utils/displayData.jsx';
-import {types} from '../../config.jsx';
+import {types, defaultReadingState} from '../../config.jsx';
 import { X } from "lucide-react";
 import { useState, useEffect } from 'react';
 
 const Stats = ({ setDisplayStats, data }) => {
+  const [numberOfItems, setNumberOfItems] = useState({});
   const [numberOfFinishedItems, setNumberOfFinishedItems] = useState({});
+  const [numberOfWantToReadItems, setNumberOfWantToReadItems] = useState({});
 
   useEffect(() => {
     const counts = {};
+    const countsFinished = {};
+    const countsWantToRead = {};
+
     types.forEach((type) => {
       counts[type] = 0;
+      countsFinished[type] = 0;
+      countsWantToRead[type] = 0;
     });
+
     data.forEach((item) => {
       const itemType = item.type;
-      console.log(itemType)
+      const itemState = item.readingState;
       if (types.includes(itemType)) {
         counts[itemType] += 1;
+
+        if(itemState == defaultReadingState){
+          countsFinished[itemType] += 1;
+        } else {
+          countsWantToRead[itemType] += 1;
+        }
       }
     });
-    setNumberOfFinishedItems(counts);
+
+    setNumberOfItems(counts);
+    setNumberOfFinishedItems(countsFinished);
+    setNumberOfWantToReadItems(countsWantToRead);
   }, [data]);
 
   return (
@@ -50,6 +67,19 @@ const Stats = ({ setDisplayStats, data }) => {
               <DisplayData
               key = {type} 
               title={`Number of ${type}${type.endsWith('s')?"":"s"}`}
+              value={numberOfItems[type]}
+              color = {"#555"}
+              />
+            );
+          })}
+        </RowContainer>
+        <RowContainer>
+          {types.map((type) => {
+            console.log(type)
+            return (
+              <DisplayData
+              key = {type} 
+              title={`Finished ${type}${type.endsWith('s')?"":"s"}`}
               value={numberOfFinishedItems[type]}
               color = {"#555"}
               />
@@ -62,21 +92,8 @@ const Stats = ({ setDisplayStats, data }) => {
             return (
               <DisplayData
               key = {type} 
-              title={`Number of ${type}${type.endsWith('s')?"":"s"}`}
-              value={numberOfFinishedItems[type]}
-              color = {"#555"}
-              />
-            );
-          })}
-        </RowContainer>
-        <RowContainer>
-          {types.map((type) => {
-            console.log(type)
-            return (
-              <DisplayData
-              key = {type} 
-              title={`Number of ${type}${type.endsWith('s')?"":"s"}`}
-              value={numberOfFinishedItems[type]}
+              title={`${type}${type.endsWith('s')?"":"s"} in wish-list`}
+              value={numberOfWantToReadItems[type]}
               color = {"#555"}
               />
             );
